434d649880a17656cb7cf940c669be79afdedfe8
Christoph Wedenig <christoph@wedenig.org>
2018-04-27 20:46:45 +0200

    Added Basic Folder structure

---
+++ Assets/Colors/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="26" columns="13">
+ <image source="../../../../Desktop/Screen Shot 2018-04-27 at 20.32.45.png" width="418" height="84"/>
+</tileset>
