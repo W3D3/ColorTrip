7ddac461eced53df9e97b42437c544cd079e110c
Christoph Wedenig <christoph@wedenig.org>
2018-04-29 01:12:58 +0200

    Enhanced GUI, added Timer

---
+++ Assets/Scripts/GUI/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+	public Text timerText;
+
+	private float startTime;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float t = Time.time - startTime;
+
+		string minutes = padZeroes(((int) t / 60).ToString(), 2);
+		string seconds = padZeroes(((int) t % 60).ToString(), 2);
+		timerText.text = minutes + ":" + seconds;
+
+	}
+
+	private string padZeroes(String numberString, int length)
+	{
+		for (int i = length - numberString.Length; i < length; i++)
+		{
+			numberString = "0" + numberString;
+		}
+
+		return numberString;
+	}
+}
