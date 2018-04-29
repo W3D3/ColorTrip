7e878ddbf0f9cd4ae036ff97bd449ed35bab639c
David <dlaubrei@gmail.com>
2018-04-29 12:21:07 +0200

    Update credits

---
+++ Assets/Scripts/GUI/CreditsHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsHandler : MonoBehaviour
+{
+
+	public Text DeathCounterText;
+	public Text TimeNeededText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		DeathCounterText.text = StatsManager.Instance.DeathCounter.ToString();
+		TimeNeededText.text = StatsManager.GetTimeFormatted(StatsManager.Instance.TimeNeeded);
+	}
+}
