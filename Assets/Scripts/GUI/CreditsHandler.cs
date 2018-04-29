c9a71b96e5b1b38867d3dabfc3e0a9e588305901
David <dlaubrei@gmail.com>
2018-04-29 12:31:37 +0200

    Update credits

--- Assets/Scripts/GUI/CreditsHandler.cs
+++ Assets/Scripts/GUI/CreditsHandler.cs
@@ -1,16 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CreditsHandler : MonoBehaviour
 {
+	private const string LB = "\r\n";
 
 	public Text DeathCounterText;
 	public Text TimeNeededText;
+	public Text DetailedStats;
 
 	// Use this for initialization
 	void Start ()
 	{
+		
+		StatsManager.Instance.PushLevel();		
+		StatsManager.Instance.PushLevel();		
+		StatsManager.Instance.PushLevel();		
+		StatsManager.Instance.PushLevel();
+		
 		DeathCounterText.text = StatsManager.Instance.DeathCounter.ToString();
 		TimeNeededText.text = StatsManager.GetTimeFormatted(StatsManager.Instance.TimeNeeded);
+		DetailedStats.text = buildDetailedStats();
+	}
+
+	private string buildDetailedStats()
+	{
+		var stats = StatsManager.Instance.GetStats();
+
+		var result = "";
+
+		for (int i = 0; i < stats.Count; i++)
+		{
+			var level = stats[i];
+			
+			result += "Level " + (i + 1) + LB;
+
+			result += "Time needed: " + StatsManager.GetTimeFormatted(level.TimeNeeded) + LB;
+			result += "Deaths: " + level.DeathCount + LB;
+			result += LB;
+		}
+
+		return result;
 	}
 }
