ff84ec3fe1fd9d6d85bd16f751b7b88125338ee2
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2018-06-21 19:26:21 +0200

    UI for main menu and level screen

---
+++ Assets/RefocusUi.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RefocusUi : MonoBehaviour
+{
+
+    private GameObject _lastSelectedGameObject;
+
+    void Start()
+    {
+        _lastSelectedGameObject = EventSystem.current.firstSelectedGameObject ?? new GameObject();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if (EventSystem.current.currentSelectedGameObject == null)
+	    {
+            EventSystem.current.SetSelectedGameObject(_lastSelectedGameObject);
+	    } else if (_lastSelectedGameObject != null && _lastSelectedGameObject != EventSystem.current.currentSelectedGameObject)
+	    {
+	        _lastSelectedGameObject = EventSystem.current.currentSelectedGameObject;
+	    }
+	}
+}
---
+++ Assets/SelectButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectButtonScript : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public string Hint;
+    public Text HintText;
+    public Vector3 OriginalPosition;
+    public Vector3 TargetPosition;
+    public Vector3 CurrentPosition;
+
+    void Start()
+    {
+        OriginalPosition = transform.position;
+        TargetPosition = transform.position;
+        HintText.text = string.Empty;
+    }
+
+    void Update()
+    {
+        CurrentPosition = transform.position;
+        var newX = Mathf.SmoothStep(CurrentPosition.x, TargetPosition.x, Time.deltaTime * 20f);
+        CurrentPosition.x = newX;
+        transform.position = CurrentPosition;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        TargetPosition = OriginalPosition + new Vector3(70, 0);
+        HintText.text = Hint;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        TargetPosition = OriginalPosition;
+    }
+}
